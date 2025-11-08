be798fe34b14b1ed72e8c371ad30f6624fc80a8b
Matheus <matheusroccogg@gmail.com>
2025-11-07 21:44:06 -0300

    env

--- conexao.py
+++ conexao.py
@@ -1,11 +1,15 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def get_conexao():
     conn = psycopg2.connect(
-        dbname="todolist",  
-        user = 'postgres',
-        password = 'admin',
-        host = '127.0.0.1',
-        port = '1024',
+        dbname=os.getenv('DB_DATABASE'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT')
     )
     return conn
